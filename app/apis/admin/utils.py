95dd29f9b32c3c6cf479d7f9501dbd2563c2c4d1
Hanno Zwikstra <hanno@hannozwikstra.nl>
2024-05-16 11:42:13 +0000

    fixed error in get_disk_usage implementation

--- app/apis/admin/utils.py
+++ app/apis/admin/utils.py
@@ -3,10 +3,13 @@ import subprocess
 def get_disk_usage(parameters: str):
 
     # Use a list for the command instead of a string
-    command = ['df', '-h', parameters]
+    command = ["df", "-h"]
     
+    if parameters:
+        command.append(parameters.split())
+
     try:
-        result = subprocess.check_output(command, shell=False)
+        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
         usage = result.stdout.strip().decode()
     except:
         raise Exception("An unexpected error was observed")
